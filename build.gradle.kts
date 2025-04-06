f358cbc1c0789a91cf07b21ad4beaf895e11d6db
yuvraj-coder1 <yuvrajshorewalavis@gmail.com>
2025-04-06 15:20:49 +0530

    fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@
 plugins {
     id ("com.android.application" )version "8.9.1" apply false
     id ("com.android.library")version "8.9.1" apply false
-    id ("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id ("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id ("com.google.gms.google-services") version "4.4.2" apply false
-    id("androidx.compose.compiler") version "1.5.15" apply false
+    id ("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+//    id("androidx.compose.compiler") version "1.4.5" apply false
 
 
 }
